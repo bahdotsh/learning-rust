fbb67d734b983a97f0355148c9ca1717989fcb63
bahdotsh <appu.yess@gmail.com>
2023-04-06 14:27:31 +0530

    moved area inside the impl of Rectangle

--- area_struct/src/main.rs
+++ area_struct/src/main.rs
@@ -3,14 +3,18 @@ struct Rectangle {
 	width: u32,
 	height: u32,
 }
-
+impl Rectangle {
+	fn area(&self) -> u32 {
+		self.width * self.height
+	}	
+}
 fn main() {
     let r1 = Rectangle {
     	width: 20,
 	height: 50,
     };
     println!("r1 is a {:?}", r1);
-    println!("Area of r1 = {}", area(&r1));
+    println!("Area of r1 = {}", r1.area());
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
